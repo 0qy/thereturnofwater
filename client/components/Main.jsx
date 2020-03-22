fb2f9abd3de0bb85d4571cea6af380b2a5a291cc
0qy <linqianye333@gmail.com>
2020-03-23 10:44:58 +1300

    window scrolling, need to work on speed

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Player, ControlBar } from 'video-react'
+import autoScroll from '../scripts/autoScroll'
 
-const Main = () => {
-  return (
-    <div className="main">
-      <video autoPlay fluid className="fullScreen">
-        <source src='/background.mp4' type='video/mp4'/>
-        {/* <ControlBar disableCompletely={true} /> */}
-      </video>
-    </div>
-  )
+class Main extends React.Component {
+  componentDidMount () {
+    window.scrollTo({
+      top: 1000,
+      left: 4000,
+      behavior: 'smooth'
+    })
+  }
+  render () {
+    return (
+      <div className="main">
+        <video autoPlay className="fullScreen">
+          <source src='/background.mp4' type='video/mp4'/>
+        </video>
+      </div>
+    )
+  }
 }
 
 export default Main
