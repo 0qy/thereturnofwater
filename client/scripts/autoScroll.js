a7abdf584d88ccd0c908e56ac98573fc8cb0a118
0qy <linqianye333@gmail.com>
2020-04-14 13:38:28 +1200

    changed some small things in css file

--- client/scripts/autoScroll.js
+++ client/scripts/autoScroll.js
@@ -1,13 +1,5 @@
-const speed = 10
-
 const scroll = () => {
-  console.log('scroll script initialised')
   window.scroll(0, 1000)
 }
-
-// const initialise = () => {
-//   console.log('autoScroll initialised')
-//   setTimeout(scroll(), 50)
-// }
 
 window.onload = scroll()
